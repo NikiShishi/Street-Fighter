f093888408c9271ba801a056654728f2b2a93fb6
JJ <jxkdfire@gmail.com>
2025-04-03 12:07:31 +0200

    Update SF.py

--- SF.py
+++ SF.py
@@ -2,7 +2,8 @@ import pyxel,time
 
 pyxel.init(300,200)
 
-global x0, y0, x1, y1, v, v1, sur_sol, sur_sol1, sol, gravite, HP_0, HP_1, gameover ,gameover1 ,cd ,last_time ,last_time1 ,damage ,shield1,xp, VITESSE_PROJECTILE ,projectile , shield ,yp,yp1 ,projectile1
+global x0, y0, x1, y1, yp, yp1 
+global v, v1, sur_sol, sur_sol1,screen, sol, gravite, HP_0, HP_1, gameover ,gameover1 ,cd ,last_time ,last_time1 ,damage ,shield1, VITESSE_PROJECTILE ,projectile , shield ,projectile1
 x0: int = 10
 y0: int = 120
 x1: int = 250
@@ -25,13 +26,12 @@ largeur:int = 70
 cd:int = 0.3
 last_time = None
 last_time1 = None
-damage = 6
-xp = 0
-xp1 = 0
+damage: int = 6
 projectile = []
 projectile1 = []
-shield = False
-shield1 = False
+shield: bool = False
+shield1: bool = False
+screen: bool = True
 
 pyxel.load("re.pyxres")
 
@@ -78,7 +78,8 @@ def hitprojectile1() -> bool :
 
 
 def update():
-    global x0, y0, x1, y1, v, v1, sur_sol, sur_sol1, sol, gravite, HP_0, HP_1,gameover ,shield1,gameover1 ,longueur, largeur ,cd,last_time ,last_time1 ,damage ,xp ,VITESSE_PROJECTILE ,projectile ,shield,yp,yp1 ,projectile1
+    global x0, y0, x1, y1, yp, yp1
+    global v, v1, sur_sol, screen,sur_sol1, sol, gravite, HP_0, HP_1,gameover ,shield1,gameover1 ,longueur, largeur ,cd,last_time ,last_time1 ,damage ,VITESSE_PROJECTILE ,projectile ,shield ,projectile1
 
     shield = False
     shield1 = False
@@ -136,119 +137,125 @@ def update():
         x1 = 0
     elif x1 > 280:
         x1 = 280
-    
+    if pyxel.btn(pyxel.KEY_RETURN):
+        screen = False 
 
 def draw():
-    global x,y, x1, y1, v, v1, sur_sol, sur_sol1, sol, gravite, HP_0, HP_1, x0, y0 ,gameover,shield1, gameover1,longueur, largeur,cd,last_time,last_time1,damage , xp , VITESSE_PROJECTILE ,projectile ,shield,yp,yp1,projectile1
-    if gameover :
-        pyxel.cls(11)
-        pyxel.text(120, 100, "Victoire du joueur 1", 2 )
+    global x0, y0, x1, y1, yp, yp1 
+    global v, v1, sur_sol,screen, sur_sol1, sol, gravite, HP_0, HP_1, gameover ,gameover1 ,cd ,last_time ,last_time1 ,damage ,shield1, VITESSE_PROJECTILE ,projectile , shield ,projectile1
+    if screen == True : 
+        pyxel.cls(1)    
+    else : 
+        if gameover :
+            pyxel.cls(11)
+            pyxel.text(120, 100, "Victoire du joueur 1", 2 )
 
         # Mise à 0 des variables
-        if pyxel.btn(pyxel.KEY_R) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_START):
-            x0 = 10
-            y0 = 120
-            x1 = 250
-            y1 = 120 
-            HP_0 = 46
-            HP_1 = 46
-            gameover = False
-            gameover1 = False
+            if pyxel.btn(pyxel.KEY_R) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_START):
+                x0 = 10
+                y0 = 120
+                x1 = 250
+                y1 = 120 
+                HP_0 = 46
+                HP_1 = 46
+                gameover = False
+                gameover1 = False
 
-    elif gameover1 :
-        pyxel.cls(4)
-        pyxel.text(120, 100, "Victoire du joueur 2", 2 )
+        elif gameover1 :
+            pyxel.cls(4)
+            pyxel.text(120, 100, "Victoire du joueur 2", 2 )
 
         # Mise à 0 des variables
-        if pyxel.btn(pyxel.KEY_R) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_START):
-            x0 = 10
-            y0 = 120
-            x1 = 250
-            y1 = 120 
-            HP_0 = 46
-            HP_1 = 46
-            gameover = False
-            gameover1 = False
-    else:
-        pyxel.cls(1)
-        pyxel.rectb(x0, y0, longueur, largeur, 1)
-        pyxel.rectb(x1, y1, longueur, largeur, 4)
-        pyxel.blt(x0,y0,0 ,0 ,0 ,46 ,70)
-        pyxel.blt(x1,y1,0,103, 2,-46 ,70)
-        pyxel.rect(x0, y0 - 30 ,HP_0, 10, 3)
-        pyxel.rectb(x0, y0 - 30 ,longueur, 10, 3)
-        pyxel.rect(x1, y1 - 30, HP_1, 10, 4)
-        pyxel.rectb(x1, y1 - 30 ,longueur, 10, 4)
+            if pyxel.btn(pyxel.KEY_R) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_START):
+                x0 = 10
+                y0 = 120
+                x1 = 250
+                y1 = 120 
+                HP_0 = 46
+                HP_1 = 46
+                gameover = False
+                gameover1 = False
+        else:
+            pyxel.cls(0)
+            pyxel.rectb(x0, y0, longueur, largeur, 1)
+            pyxel.rectb(x1, y1, longueur, largeur, 4)
+            pyxel.blt(x0,y0,0 ,0 ,0 ,46 ,70)
+            pyxel.blt(x1,y1,0,103, 2,-46 ,70)
+            pyxel.rect(x0, y0 - 30 ,HP_0, 10, 3)
+            pyxel.rectb(x0, y0 - 30 ,longueur, 10, 3)
+            pyxel.rect(x1, y1 - 30, HP_1, 10, 4)
+            pyxel.rectb(x1, y1 - 30 ,longueur, 10, 4)
 
-        if pyxel.btn(pyxel.GAMEPAD1_BUTTON_A):
-            shield1 = True
-            pyxel.rect(x0 + 46, y0 , 10,70, 10)
+            if pyxel.btn(pyxel.GAMEPAD1_BUTTON_A):
+                shield1 = True
+                pyxel.rect(x0 + 46, y0 , 10,70, 10)
             
         
-        if pyxel.btn(pyxel.KEY_T):
-            shield = True
-            pyxel.rect(x1 - 10, y1 , 10,70, 10)
+            if pyxel.btn(pyxel.KEY_T):
+                shield = True
+                pyxel.rect(x1 - 10, y1 , 10,70, 10)
 
-        if pyxel.btnp(pyxel.GAMEPAD1_BUTTON_X):
-            if len(projectile) < 1 :
-                projectile.append(x0)
-                yp = y0
-        for t in projectile:
-            if projectile[0] < 280 :
-                projectile[0] += 2
-                pyxel.rect(projectile[0], yp + 20, 20, 10 , 1)
-                pyxel.blt(projectile[0] , yp + 20 , 0 ,213,8,20,12)
-                if (shield == True and projectile[0] + 20 == x1 ):
-                    projectile.remove(projectile[0])
-                if hitprojectile():
-                    HP_1 -= damage
-                    projectile.remove(projectile[0])
-                    if HP_1 <= 0 :
-                        gameover = True       
-            else :
-                projectile.remove(t)
+            if pyxel.btnp(pyxel.GAMEPAD1_BUTTON_X):
+                if len(projectile) < 1 :
+                    projectile.append((x0 + longueur))
+                    yp = y0
+            for t in projectile:
+                if projectile[0] < 280 :
+                    projectile[0] += VITESSE_PROJECTILE
+                    pyxel.rect(projectile[0], yp + 20, 20, 10 , 1)
+                    pyxel.blt(projectile[0] , yp + 20 , 0 ,213,8,20,12)
+                    if (shield == True and projectile[0] + 20 == x1 ):
+                        projectile.remove(projectile[0])
+                    if hitprojectile():
+                        HP_1 -= (damage - 2)
+                        projectile.remove(projectile[0])
+                        if HP_1 <= 0 :
+                            gameover = True       
+                else :
+                    projectile.remove(t)
             
-        if pyxel.btnp(pyxel.KEY_X):
-            if len(projectile1) < 1 :
-                projectile1.append(x1)
-                yp1 = y1
-        for tir in projectile1:
-            if projectile1[0] > 0 :
-                projectile1[0] -= 2
-                pyxel.rect(projectile1[0], yp1 + 20, 20, 10 , 1)
-                pyxel.blt(projectile1[0] , yp1 + 20 , 0 ,216,25,21,15)
-                if (shield1 == True and (projectile1[0] <= x0 + longueur + 20 )):
-                    projectile1.remove(projectile1[0])
-                if hitprojectile1():
-                    HP_0 -= damage
-                    projectile1.remove(projectile1[0])
+            if pyxel.btnp(pyxel.KEY_X):
+                if len(projectile1) < 1 :
+                    projectile1.append((x1 - 20))
+                    yp1 = y1
+            for tir in projectile1:
+                if projectile1[0] > 0 :
+                    projectile1[0] -= VITESSE_PROJECTILE
+                    pyxel.rect(projectile1[0], yp1 + 20, 20, 10 , 1)
+                    pyxel.blt(projectile1[0] , yp1 + 20 , 0 ,216,25,21,15)
+                    if (shield1 == True and (projectile1[0] <= x0 + longueur + 20 )):
+                        projectile1.remove(projectile1[0])
+                    if hitprojectile1():
+                        HP_0 -= (damage - 2)
+                        projectile1.remove(projectile1[0])
+                        if HP_0 <= 0 :
+                            gameover1 = True   
+                else :
+                    projectile1.remove(tir)
+
+            # Attaque 1 manette
+            if pyxel.btnp(pyxel.GAMEPAD1_BUTTON_B):
+                current_time = time.time()
+                if last_time == None or (current_time - last_time) > cd:
+                    last_time = current_time
+                    if hit(x0 , y0):
+                        HP_1 = HP_1 - damage
+                        pyxel.rect(x1, y1 - 30 ,HP_1, 10, 4)
+                        print(HP_1)
+                    if HP_1 <= 0 :
+                        gameover = True
+            # Attaque 1 clavier  
+                    
+            if pyxel.btnp(pyxel.KEY_SPACE):
+                current_time1 = time.time()
+                if last_time1 == None or (current_time1 - last_time1) > cd:
+                    last_time1 = current_time1
+                    if hit1(x1, y1):
+                        HP_0 = HP_0 - damage
+                        print(HP_0)
                     if HP_0 <= 0 :
-                        gameover1 = True   
-            else :
-                projectile1.remove(tir)
-
-        # Attaque 1 manette
-        if pyxel.btnp(pyxel.GAMEPAD1_BUTTON_B):
-            current_time = time.time()
-            if last_time == None or (current_time - last_time) > cd:
-                last_time = current_time
-                if hit(x0 , y0):
-                    HP_1 = HP_1 - damage
-                    pyxel.rect(x1, y1 - 30 ,HP_1, 10, 4)
-                    print(HP_1)
-                if HP_1 <= 0 :
-                    gameover = True
-        # Attaque 1 clavier  
-                
-        if pyxel.btnp(pyxel.KEY_SPACE):
-            current_time1 = time.time()
-            if last_time1 == None or (current_time1 - last_time1) > cd:
-                last_time1 = current_time1
-                if hit1(x1, y1):
-                    HP_0 = HP_0 - damage
-                    print(HP_0)
-                if HP_0 <= 0 :
-                    gameover1 = True
-
+                        gameover1 = True
+        
    
 pyxel.run(update, draw)
+
